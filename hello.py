acf5b34c7044dd17af8d5e5c142f422546f86001
AtmaDharma-dasa <atmadharma.das@gmail.com>
2023-05-06 21:46:51 +1200

    input

--- hello.py
+++ hello.py
@@ -3,4 +3,15 @@ print("hello world")
 def add(num1,num2):
    return num1+num2
 
-print(add(100,8))
+x = int(input("Enter num1:  "))
+y = int(input("Enter num12:  "))
+
+result = add(x,y)
+print_result = input("Do you want to print the result? (Y/N): ")
+
+# Conditionally print the result
+if print_result.lower() == "y":
+    print(result)
+    print("working")
+
+else: print("not working")
